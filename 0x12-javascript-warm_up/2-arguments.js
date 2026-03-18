5aeaea427e4e767e0b7a79157a1107a15e865465
Bobchri <chrixclout@gmail.com>
2026-03-18 01:21:52 -0700

    added peoper spacing using semistandard style in  script that prints a message depending of the number of arguments passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,17 +2,10 @@
 
 const args = process.argv.length - 2;
 
-if (args === 0)
-{
-	console.log('No argument');
+if (args === 0) {
+  console.log('No argument');
+} else if (args === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-else if (args === 1)
-{
-	console.log('Argument found');
-}
-else
-{
-	console.log('Arguments found');
-}
-
-
